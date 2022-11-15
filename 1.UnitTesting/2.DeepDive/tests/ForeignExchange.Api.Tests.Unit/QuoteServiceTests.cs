f7e029425e231843e3b858c410948b5e905ed176
Matthew Wasson <MatthewWasson@eurofinsUS.com>
2022-11-15 13:08:53 -0600

    More things

---
+++ 1.UnitTesting/2.DeepDive/tests/ForeignExchange.Api.Tests.Unit/QuoteServiceTests.cs
@@ -0,0 +1,34 @@
+﻿namespace ForeignExchange.Api.Tests.Unit;
+
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Eurofins.UsFood.Testing.XunitAttributes;
+using FluentAssertions;
+using Services;
+using Xunit;
+
+[Trait("Category", "QuoteService")]
+public class QuoteServiceTests
+{
+  private IFixture _fixture;
+
+  public void TestInitialize()
+  {
+    _fixture = new Fixture();
+    _fixture.Customize(new AutoMoqCustomization { ConfigureMembers = true });
+  }
+
+  [Theory, AutoMoqData]
+  public async Task GetQuote_DoStuff(QuoteService sut)
+  {
+    //Actors
+
+
+    //Acts
+    var actual = await sut.GetQuoteAsync("USD", "GBP", 9);
+
+    //Asserts
+    actual.Should().Be(9);
+  }
+}
+
