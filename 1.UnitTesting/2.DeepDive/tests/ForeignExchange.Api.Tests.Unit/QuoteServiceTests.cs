14277b28251baea532a28bcf0335d7f1fddbd66f
Nick Chapsas <nick.chapsas@gmail.com>
2022-11-15 13:15:05 -0600

    more tests

--- 1.UnitTesting/2.DeepDive/tests/ForeignExchange.Api.Tests.Unit/QuoteServiceTests.cs
+++ 1.UnitTesting/2.DeepDive/tests/ForeignExchange.Api.Tests.Unit/QuoteServiceTests.cs
@@ -1,13 +1,11 @@
 using FluentAssertions;
-using ForeignExchange.Api.Database;
 using ForeignExchange.Api.Logging;
 using ForeignExchange.Api.Models;
 using ForeignExchange.Api.Repositories;
 using ForeignExchange.Api.Services;
 using ForeignExchange.Api.Validation;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
 using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 using Xunit;
 
 namespace ForeignExchange.Api.Tests.Unit;
@@ -24,38 +22,28 @@ public class QuoteServiceTests
             _ratesRepository, _logger);
     }
     
-    [Fact]
-    public async Task GetQuoteAsync_ShouldReturnQuote_WhenCurrenciesAreValid()
+    [Theory]
+    [InlineData("GBP", "EUR", 100, 1.6, 160)]
+    public async Task GetQuoteAsync_ReturnsQuote_WhenCurrenciesAreValid(
+        string fromCurrency, string toCurrency, decimal amount, decimal rate, decimal final)
     {
         // Arrange
-        var fromCurrency = "USD";
-        var toCurrency = "GBP";
-        var amount = 100;
-        var expectedQuote = new ConversionQuote
-        { 
-            BaseAmount = amount,
-            QuoteCurrency = toCurrency,
-            BaseCurrency = fromCurrency,
-            QuoteAmount = 160
-        };
-        
         var expectedRate = new FxRate
         {
             FromCurrency = fromCurrency,
             ToCurrency = toCurrency,
             TimestampUtc = DateTime.UtcNow,
-            Rate = 1.6m
+            Rate = rate
         };
-
-        _ratesRepository
-            .GetRateAsync(fromCurrency, toCurrency)
+        
+        _ratesRepository.GetRateAsync(fromCurrency, toCurrency)
             .Returns(expectedRate);
-
+        
         // Act
         var result = await _sut.GetQuoteAsync(fromCurrency, toCurrency, amount);
 
         // Assert
-        result.Should().BeEquivalentTo(expectedQuote);
+        result!.QuoteAmount.Should().Be(final);
     }
 
     [Fact]
@@ -109,5 +97,50 @@ public class QuoteServiceTests
         _logger.Received(1).LogInformation("Retrieved quote for currencies {FromCurrency}->{ToCurrency} in {ElapsedMilliseconds}ms",
             Arg.Is<object[]>(x => x[0].ToString() == fromCurrency &&
                                   x[1].ToString() == toCurrency ));
+    }
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-5)]
+    public async Task GetQuoteAsync_ThrowsException_WhenAmountIsZeroOrNegative(decimal amount)
+    {
+        // Arrange
+        var fromCurrency = "GBP";
+        var toCurrency = "USD";
+        var expectedRate = new FxRate
+        {
+            FromCurrency = fromCurrency,
+            ToCurrency = toCurrency,
+            TimestampUtc = DateTime.UtcNow,
+            Rate = 1.6m
+        };
+        
+        _ratesRepository.GetRateAsync(fromCurrency, toCurrency)
+            .Returns(expectedRate);
+        
+        // Act
+        var quoteAction = () => _sut.GetQuoteAsync(fromCurrency, toCurrency, amount);
+
+        // Assert
+        await quoteAction.Should().ThrowAsync<NegativeAmountException>()
+            .WithMessage("You can only convert a positive amount of money");
+    }
+    
+    [Fact]
+    public async Task GetQuoteAsync_ShouldReturnNull_WhenNoRateExists()
+    {
+        // Arrange
+        var fromCurrency = "GBP";
+        var toCurrency = "USD";
+        var amount = 100;
+
+        _ratesRepository.GetRateAsync(fromCurrency, toCurrency)
+            .ReturnsNull();
+        
+        // Act
+        var quote = await _sut.GetQuoteAsync(fromCurrency, toCurrency, amount);
+
+        // Assert
+        quote.Should().BeNull();
     }
 }
